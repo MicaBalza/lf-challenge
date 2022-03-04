c1c888d579674b291189e441c51dad8e8ce053b6
MicaBalza <mica.balza@gmail.com>
2022-03-03 22:50:33 -0300

    feat: add button component

--- src/constants/imagesConfig.ts
+++ src/constants/imagesConfig.ts
@@ -1,6 +1,6 @@
-export const IMG_BASE_URL = 'http://image.tmdb.org/t/p/';
+export const IMG_URL = 'http://image.tmdb.org/t/p/';
 
-export const BACKDROP_SIZES = {
+export const IMG_SIZES = {
   w300: 'w300',
   w780: 'w780',
   w1280: 'w1280',
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,30 @@
 import { useFetch } from '../../hooks/useFetch';
 
+import Button from '../../components/Button';
+
 import styles from './index.module.scss';
 
-import { IMG_BASE_URL, BACKDROP_SIZES } from '../../constants/imagesConfig';
+import { IMG_URL, IMG_SIZES } from '../../constants/imagesConfig';
 
 const Home = () => {
-  
-  const { data} = useFetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API_KEY}`);
+  const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_TMDB_API_KEY}`;
+  const { data} = useFetch(url);
   
   return (
     <>
       { data &&
         <div className={styles.home}>
-          <img src={`${IMG_BASE_URL}${BACKDROP_SIZES.original}/${data.results[0].backdrop_path}`} className={styles.backgroundImg} />
-          <p className={styles.subheader}>Original de <span className="bold">Liteflix</span></p>
-          <p className={styles.title}>{data.results[0].title}</p>
+          <img
+            src={`${IMG_URL}${IMG_SIZES.original}/${data.results[0].backdrop_path}`} className={styles.backgroundImg}
+          />
+          <div className={styles.container}>
+            <p className={styles.subheader}>Original de <span className="bold">Liteflix</span></p>
+            <p className={styles.title}>{data.results[0].title}</p>
+            <div>
+              <Button text='Reproducir' />
+              <Button text='Reproducir' variant='outlined' className={styles.rightButton} />
+            </div>
+          </div>
         </div>
       }
     </>
