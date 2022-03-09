641766415d42fdc5251abe532b4e802196911eb7
MicaBalza <mica.balza@gmail.com>
2022-03-08 21:37:06 -0300

    style: file input

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -3,8 +3,8 @@ import styles from './index.module.scss';
 const FileInput = () => {
   return (
     <label className={styles.input}>
-      <input type="file" accept="image/png, image/jpeg" hidden/>
-      <span className="bold">Agregá un archivo</span> o arrastralo y soltalo aquí
+      <input type="file" accept="image/png, image/jpeg"/>
+      <span className="bold">Agregá un archivo </span>o arrastralo y soltalo aquí
     </label>
   );
 };
