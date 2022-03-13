fc81e1c50c052c26e363d336d2e75e32c9224cae
MicaBalza <mica.balza@gmail.com>
2022-03-13 13:26:11 -0300

    feat: progress bar component

---
+++ src/components/ProgressBar/index.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import styles from './index.module.scss';
+
+
+const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+  const timer = setInterval(() => {
+    setProgress((prevProgress) => (prevProgress >= 100 ? 100 : prevProgress + 1));
+  }, 50);
+  return () => {
+    clearInterval(timer);
+  };
+  }, []);
+
+  return (
+    <>
+        <p className={styles.label}>Cargando <span className="bold">{`${progress}%`}</span></p>
+        <div className={styles.containerStyles}>
+          <div className={styles.fillerStyles} style={{ width: `${progress}%` }}>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default ProgressBar
