9cb5d4f316fa387088e3d6730a176bbb5667797d
MicaBalza <mica.balza@gmail.com>
2022-03-14 00:23:45 -0300

    style: responsive dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -7,8 +7,8 @@ import styles from './index.module.scss';
 interface Props {
   className?: string;
   options: {
-      text: string,
-      value: string
+    text: string;
+    value: string;
   }[];
   setOption: (option: string) => void;
 }
@@ -18,10 +18,11 @@ const Dropdown = ({ className, options, setOption }: Props) => {
   const [showDropdown, setshowDropdown] = useState(false);
 
   const handleShowDropdown = () => {
+    console.log('click');
     setshowDropdown(!showDropdown);
   };
 
-  const handleOptionClick = (option: any) => {
+  const handleOptionClick = (option: string) => {
     setDropdownOption(option);
     setOption(option);
     handleShowDropdown();
@@ -30,19 +31,36 @@ const Dropdown = ({ className, options, setOption }: Props) => {
   return (
     <div className={`${styles.container} ${className}`}>
       <div onClick={handleShowDropdown}>
-        <p>Ver:
-          <span className={`${styles.label} bold`}> {options[options.findIndex(opt => opt.value === dropdownOption)]?.text}</span>
+        <p>
+          Ver:
+          <span className={`${styles.label} bold`}>
+            {' '}
+            {
+              options[options.findIndex((opt) => opt.value === dropdownOption)]
+                ?.text
+            }
+          </span>
         </p>
       </div>
-      { showDropdown && <div className={styles.dropdown}>
-        { options.map(opt => (
-          <div key={opt.value} onClick={() => handleOptionClick(opt.value)} className={styles.option}>
-            <label htmlFor={opt.value} className={opt.value === dropdownOption ? 'bold' : ''}>{opt.text}</label>
-            <input type="radio" name="movieGroup" value={opt.value} id={opt.value} hidden />
-            {opt.value === dropdownOption && <img src={check}/>}
-          </div>
-        )) }
-      </div> }
+      {showDropdown && (
+        <div className={styles.dropdown}>
+          {options.map((opt) => (
+            <div
+              key={opt.value}
+              onClick={() => handleOptionClick(opt.value)}
+              className={styles.option}
+            >
+              <p className={opt.value === dropdownOption ? 'bold' : ''}>
+                {opt.text}
+              </p>
+
+              {opt.value === dropdownOption && (
+                <img src={check} alt="Seleccionado" />
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
